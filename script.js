326ede9a9462cafe77cad10670d82f4fd27160f4
Shivdasbhande05 <shivdasbhande@gmail.com>
2024-12-30 22:42:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function getFormvalue() {
     //Write your code here
+	let firstName = document.getElementById("fname");
+	let lastName = document.getElementById("lname");
+	let submitForm = document.getElementById("form1");
+
+	submitForm.onsubmit = (event) => {
+		event.preventDefault();
+		let fName = firstName.value;
+		let lName = lastName.value;
+		alert (fName + " " + lName);
+	}
+
+	
 
 }
